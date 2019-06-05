395462fac4c31ca1167b3ee0d8ec5934828c879d
Aram Greenman <aram@karat.io>
2019-06-05 14:52:39 -0700

    Fix conflicting value-list rules

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = {
   ],
   rules: {
     "scss/dollar-variable-default": null,
+    "scss/at-function-named-arguments": null,
     "string-quotes": "single",
+    "value-list-comma-space-after": "always-single-line",
+    "value-list-comma-newline-after": "always-multi-line",
   },
 }
